73ce08ab53b8844bf2c94dd62f249f97704a2a17
Aadish Chopra <30322542+aadishchopra@users.noreply.github.com>
2017-09-07 15:57:13 -0500

    Web scraping done through rsdriver
    Web scraper built on R using the CSS selector

---
+++ RSele.R
@@ -0,0 +1,23 @@
+install.packages("RSelenium")
+library("RSelenium")
+remDr<-rsDriver(port = 4444L,browser = "chrome",version = "latest",chromever = "latest",geckover = "latest",check = TRUE)
+Client<-remDr$client
+Client$open()
+Client$navigate("https://www.twitter.com")
+
+
+for(i in 1:1000)
+
+{  
+  
+Css<-Client$findElements(using = "css selector",value = ".tweet-text")
+CSS_Text_Headers<- unlist(lapply(Css, function(x){x$getElementText()}))
+CSS_Text_Headers 
+Client$sendKeysToActiveElement(list(key = "end"))
+Css<-Client$findElements(using = "css selector",value = ".tweet-text")
+CSS_Text_Headers<- unlist(lapply(Css, function(x){x$getElementText()}))
+CSS_Text_Headers 
+
+}
+
+
