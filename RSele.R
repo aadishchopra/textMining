12b26653c31df8d45e0832c3480169edfcbaee42
Aadish Chopra <30322542+aadishchopra@users.noreply.github.com>
2017-09-12 17:15:09 -0500

    handled few bugs in this version

--- RSele.R
+++ RSele.R
@@ -1,24 +1,27 @@
 install.packages("RSelenium")
 library("RSelenium")
+# important because it downloads all the things(geckodriver,chrome driver,phantom js)
+
 remDr<-rsDriver(port = 4444L,browser = "chrome",version = "latest",chromever = "latest",geckover = "latest",check = TRUE)
 Client<-remDr$client
 Client$open()
-Client$navigate("https://www.twitter.com")
 
+# list of different handles which you want to extract
+Client$navigate("https://www.twitter.com/LaSalleCoHealth")
 
-for(i in 1:1000)
+# escape is required to dismiss the login alert
 
+Client$sendKeysToActiveElement(list(key="escape"))
+
+for(i in 1:50)
 {  
-  
+# required for getting tweets from the page which are not loaded
+  Client$sendKeysToActiveElement(list(key = "end"))
 Css<-Client$findElements(using = "css selector",value = ".tweet-text")
-CSS_Text_Headers<- unlist(lapply(Css, function(x){x$getElementText()}))
-CSS_Text_Headers 
-Client$sendKeysToActiveElement(list(key = "end"))
-Css<-Client$findElements(using = "css selector",value = ".tweet-text")
-CSS_Text_Headers<- unlist(lapply(Css, function(x){x$getElementText()}))
-CSS_Text_Headers 
-
 }
+CSS_DATA_FRAME<- unlist(lapply(Css, function(x){x$getElementText()}))
+CSS_DATA_FRAME<-as.data.frame(CSS_DATA_FRAME)
+Client$close()
 
 
 
